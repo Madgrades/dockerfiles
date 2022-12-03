87f7037bd9d33ff1fb4611ee94013d145860dc93
Keenan Thompson <keenan@keenant.com>
2022-12-03 00:53:05 -0800

    blah

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,15 @@ echo "Current fetch-seed-data digest: $FETCH_SEED_DATA_DIGEST_CURRENT"
 echo "Pulling latest versions..."
 docker compose pull
 
+echo "Starting latest fetch-seed-data..."
+docker compose up fetch-seed-data
+
 FETCH_SEED_DATA_IMAGE_UPDATED=$(docker compose images $FETCH_SEED_DATA_CONTAINER -q)
 FETCH_SEED_DATA_DIGEST_UPDATED=$(docker inspect --format='{{index .RepoDigests 0}}' $FETCH_SEED_DATA_IMAGE_UPDATED)
 echo "Updated fetch-seed-data digest: $FETCH_SEED_DATA_DIGEST_UPDATED"
 
 echo "Running migrations..."
 docker compose up migrate
-
-echo "Starting latest fetch-seed-data..."
-docker compose up fetch-seed-data
 
 if [ "$FETCH_SEED_DATA_DIGEST_CURRENT" == "$FETCH_SEED_DATA_DIGEST_UPDATED" ]; then
   echo "Skipping seed, no new seed data available"
