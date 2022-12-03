573aaf6776fad9f0a9d79d869a636d19ed3d9dd8
Keenan Thompson <keenan@keenant.com>
2022-12-03 00:56:01 -0800

    Fetch seed data from docker image. (#1)

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+FETCH_SEED_DATA_CONTAINER=fetch-seed-data
+
+FETCH_SEED_DATA_IMAGE_CURRENT=$(docker compose images $FETCH_SEED_DATA_CONTAINER -q)
+FETCH_SEED_DATA_DIGEST_CURRENT=$(docker inspect --format='{{index .RepoDigests 0}}' $FETCH_SEED_DATA_IMAGE_CURRENT)
+echo "Current fetch-seed-data digest: $FETCH_SEED_DATA_DIGEST_CURRENT"
+
+echo "Pulling latest versions..."
+docker compose pull
+
+echo "Starting latest fetch-seed-data..."
+docker compose up fetch-seed-data
+
+FETCH_SEED_DATA_IMAGE_UPDATED=$(docker compose images $FETCH_SEED_DATA_CONTAINER -q)
+FETCH_SEED_DATA_DIGEST_UPDATED=$(docker inspect --format='{{index .RepoDigests 0}}' $FETCH_SEED_DATA_IMAGE_UPDATED)
+echo "Updated fetch-seed-data digest: $FETCH_SEED_DATA_DIGEST_UPDATED"
+
+echo "Running migrations..."
+docker compose up migrate
+
+if [ "$FETCH_SEED_DATA_DIGEST_CURRENT" == "$FETCH_SEED_DATA_DIGEST_UPDATED" ]; then
+  echo "Skipping seed, no new seed data available"
+else
+  echo "Running seed with new seed data..."
+  docker compose up seed
+fi
+
+echo "Stopping containers..."
+docker compose stop
+
+echo "Starting api..."
+docker compose up -d api
+echo "Done"
